dbd9b09c4bd2b9181093b481a026f19a1a7097f0
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-02-28 15:41:51 +0000

    adding some stock charts

--- docs/page1.fsx
+++ docs/page1.fsx
@@ -4,3 +4,20 @@
 
 printfn "hello world from F#"
 (***include-fsi-output***)
+
+#load "YahooFinance.fsx"
+
+open YahooFinance
+
+let vti = YahooFinance.PriceHistory("vti")
+
+#r "nuget: Plotly.NET, 2.0.0-preview.17"
+
+open Plotly.NET
+
+
+vti
+|> List.map (fun x -> x.Date, x.AdjustedClose)
+|> Chart.Line
+|> GenericChart.toChartHTML
+(***include-it-raw***)
